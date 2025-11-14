1f47db77175a07576f3520e0f48164484c9a8343
mr-leo44 <lionnelkaniki44@gmail.com>
2025-11-14 08:46:29 +0100

    feat(users): create UsersModule with UsersService and Prisma integration

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [UsersService],
-  controllers: [UsersController]
+  exports: [UsersService], // <-- très important pour AuthModule
 })
 export class UsersModule {}
