6bf2f58ffa7a1e0c376bec766fdc979a9cf572e4
mr-leo44 <lionnelkaniki44@gmail.com>
2025-11-14 08:55:32 +0100

    feat(app): COmpanyModule import from AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { CompanyModule } from './company/company.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule, AuthModule],
+  imports: [PrismaModule, UsersModule, AuthModule, CompanyModule],
   controllers: [],
   providers: [],
 })
