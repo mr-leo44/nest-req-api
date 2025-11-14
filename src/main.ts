2e723e29c93057ea4c391befc86af75b9f7524d8
mr-leo44 <lionnelkaniki44@gmail.com>
2025-11-14 07:07:34 +0100

    chore: setup global project structure, prisma service, swagger and core dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { swaggerConfig } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const swaggerDoc = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('/docs', app, swaggerDoc);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
