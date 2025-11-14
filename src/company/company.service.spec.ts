3e2bc51310cdbcdcbabc96352f13449af9bfc768
mr-leo44 <lionnelkaniki44@gmail.com>
2025-11-14 08:52:55 +0100

    add company basic test

---
+++ src/company/company.service.spec.ts
@@ -0,0 +1,19 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { CompanyService } from '../company/company.service';
+import { PrismaService } from '../prisma/prisma.service';
+
+describe('CompanyService', () => {
+  let service: CompanyService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [CompanyService, PrismaService],
+    }).compile();
+
+    service = module.get<CompanyService>(CompanyService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
